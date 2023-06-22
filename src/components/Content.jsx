23791c910786c96b39506975b4f005c1ff19724e
ahkrolina <ahkrolina@gmail.com>
2023-06-22 14:34:06 -0300

    Resolvendo os erros

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,7 +14,8 @@ export function Content() {
   const [citacao, setCitacao] = useState('')
   const [imagem, setImagem] = useState('')
   const [success, setSuccess] = useState(false)
-  const baseURL = 'https://back-end-3d9t.onrender.com/mulheres'  useEffect(() => {
+  const baseURL = 'https://back-end-3d9t.onrender.com/mulheres'; 
+   useEffect(() => {
     async function getData() {
       const response = await Axios.get(baseURL)
       setRepositories(response.data)
